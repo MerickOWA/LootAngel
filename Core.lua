b425159fb0258c3abb96f6ce4691d852a71e8085
Lowell Smith <lowell@farmworks.com>
2017-12-30 16:50:51 -0500

    Added basic roll parsing

--- Core.lua
+++ Core.lua
@@ -12,4 +12,28 @@ for i = 1, NUM_CHAT_WINDOWS do
 end
 -------------------------------------------------------------
 
-print("Loot Angel Loaded!")
+local RANDOM_ROLL_PATTERN = RANDOM_ROLL_RESULT:gsub("[().%%+-*?[%]^$]", "%%%1"):gsub("%%%%s", "(.+)"):gsub("%%%%d", "(%%d+)")
+
+function LootAngelFrame_OnLoad(self)
+	self:RegisterEvent("ADDON_LOADED")
+	self:RegisterEvent("CHAT_MSG_SYSTEM")
+end
+
+function LootAngelFrame_OnEvent(self, event, arg1)
+	if  (event == "ADDON_LOADED" and arg1 == "LootAngel") then
+		self:UnregisterEvent("ADDON_LOADED")
+		LootAngel_OnLoad()
+	elseif (event == "CHAT_MSG_SYSTEM") then
+		LootAngel_CHAT_MSG_SYSTEM(arg1)
+	end
+end
+
+function LootAngel_OnLoad()
+	print("Loot Angel Loaded!")
+end
+
+function LootAngel_CHAT_MSG_SYSTEM(msg)
+	for name, roll, low, high in msg:gmatch(RANDOM_ROLL_PATTERN) do
+		print(name..": Rolled a "..roll.." using from "..low.." to "..high)
+	end
+end
