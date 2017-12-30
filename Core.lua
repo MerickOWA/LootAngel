2cf42ed108b9173e64f1d047e86cee11d21ddfc7
Lowell Smith <lowell@farmworks.com>
2017-12-30 17:38:28 -0500

    Added history tracking

--- Core.lua
+++ Core.lua
@@ -13,10 +13,14 @@ end
 -------------------------------------------------------------
 
 local RANDOM_ROLL_PATTERN = RANDOM_ROLL_RESULT:gsub("[().%%+-*?[%]^$]", "%%%1"):gsub("%%%%s", "(.+)"):gsub("%%%%d", "(%%d+)")
+local rollHistory = {}
+local rollCounts = {}
 
 function LootAngelFrame_OnLoad(self)
 	self:RegisterEvent("ADDON_LOADED")
 	self:RegisterEvent("CHAT_MSG_SYSTEM")
+	rollHistory = {}
+	rollCounts = {}
 end
 
 function LootAngelFrame_OnEvent(self, event, arg1)
@@ -34,6 +38,30 @@ end
 
 function LootAngel_CHAT_MSG_SYSTEM(msg)
 	for name, roll, low, high in msg:gmatch(RANDOM_ROLL_PATTERN) do
-		print(name..": Rolled a "..roll.." using from "..low.." to "..high)
+		LootAngel_OnRoll(name, roll, low, high)
+	end
+end
+
+function LootAngel_OnRoll(name, roll, low, high)
+
+	local count = rollCounts[name] and rollCounts[name] + 1 or 1
+
+	table.insert(rollHistory, {
+		name = name,
+		roll = tonumber(roll),
+		low = tonumber(low),
+		high = tonumber(high),
+		count = count
+	})
+	rollCounts[name] = count
+
+	LootAngel_UpdateUI()
+end
+
+function LootAngel_UpdateUI()
+	table.sort(rollHistory, function(a, b) return a.roll > b.roll end)
+
+	for i, data in pairs(rollHistory) do
+		print(data.name..": Rolled a "..data.roll.." using from "..data.low.." to "..data.high.." for the "..data.count.." time")
 	end
 end
